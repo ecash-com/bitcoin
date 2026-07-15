41d3a6d3964c22dd839d7a879ec9124f49cbac4c
cryptaxe <cryptaxe@gmail.com>
2026-07-15 09:20:03 -0700

    Add test set of repo transactions

---
+++ src/repo_txns.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2026 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#include <set>
+
+#include <uint256.h>
+
+static const std::set<uint256> setRepurposeTx = {
+    uint256("84f12070d35bec1b6505556f1673ba030066024b7a3af8846ca2cb9b1609ee81"),
+    uint256("58147f523accaeb5b6f8da7163503fe0bb4613062bf2fe0bf82f05cc7c759c9e"),
+    uint256("84c22c4dd3d79175cce4915dc4ae95c279ff5c4a10e42c2c6e6f6c7b7fe4f762"),
+    uint256("a7e712855b83894d1dab0bb5bfb0f917d37d19dd5ade7fcf8cd73fb264de3620"),
+    uint256("63e5e211701dd8e54f7771bf3ca42c5d95f4babc5c0b14d215d03b29e2b50aa5"),
+    uint256("d46c27f772918eb22f9f9341885149a8e8f688daaf06896fe57234422434144f"),
+    uint256("b70062780880234fb05870f68da91491480f6eb64f20722d943a82f02a6edbba"),
+    uint256("ac6b191fbda77ea2114168c0f510c8db4e630c1445a7f4c4d476d5ca2f89bcf5"),
+    uint256("d55c778e36998cc367ace8e548bf6c5aff1f26f9161b749554f0e4323b1fe4dd"),
+    uint256("b234ec2b04505332f957db3fb36ea38e34eb0ed7e0e97e3ee81aca74a99c56b1"),
+    uint256("d53fd7cb94a0edec8560c962da425efe9be48ab6e615ec5558b787fee6572168"),
+    uint256("3473417b5bb3f3b108f106f10e485635d35172462bfc4a8fb4a079e30aa2b1c2"),
+    uint256("d544c70915f5fdb35a21dd14cfb378c386f09313e9cd4646e7aa4757d6f9cd26"),
+    uint256("95ca1baf731c8361b1cad0d8cefa129370e9d54852f1ef52c491baea59978db9"),
+    uint256("f1143899503c0d3ac42c42e1c4ee016a96f0577450f3e48f7814e9672d68a03e"),
+    uint256("6b7e21429179d7a8971af7d7d6e016cd68bb022e37de2f787ea17f44213032f5"),
+};
